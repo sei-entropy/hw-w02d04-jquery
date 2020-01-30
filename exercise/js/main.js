dc423c3c6310a41bd579e9f40ff79b82367a383a
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-01-30 13:05:20 +0300

    done

--- exercise/js/main.js
+++ exercise/js/main.js
@@ -14,12 +14,15 @@
  * We've started you off, ONLY replace everything instance of "replaceMe" with correct code.
  */
 
+
 function question1() {
-	$("replaceMe").css("replaceMe", "replaceMe");
-}
+	$(".wrapper").css("color", "blue");
+};
+
+$("#button1").click(question1);
 
 //$("#button1").on('click', question1);
-("#button1").click(question1);
+
 
 /**
  * Question 2:
@@ -27,11 +30,11 @@ function question1() {
  * Uncomment the code below and solve the problem
  */
 
-	// function question2(){
-	// 	$("replaceMe").replaceMe("replaceMe");
-	// }
+	 function question2(){
+		$("h2").text("Sara Kuddah");
+	 }
 
-	// $("#button1").on('click', question2);
+	 $("#button1").on('click', question2);
 
 /**
  * Question 3:
@@ -40,11 +43,11 @@ function question1() {
  * Uncomment the code below and solve the problem
  */
 
-	// function question3(){
-	// 	$("replaceMe").html("replaceMe");
-	// }
+	 function question3(){
+	 	$(".falseFact").html("True Fact");
+	 }
 
-	// $("#button1").on('click', question3);
+	 $("#button1").on('click', question3);
 
 
 //SECTION 2: Now you are on your own. Write the necessary code to accomplish the following.
@@ -55,19 +58,36 @@ function question1() {
  */
 
 
+function question4() {
+	$("body").css("backgroundColor", "pink");
+};
+
+
+$("#button2").click(question4);
 
 /**
  * Question 5:
  * When button 2 is clicked, change the color of all h2's to "green"
  */
 
+function question5() {
+	$("h2").css("color", "green");
+};
 
+
+$("#button2").click(question5);
 
 /**
  * Question 6:
  * When button 2 is clicked, change the html content of all "blockquotes" to be "<span>no quote</span>"
  */
 
+function question6() {
+	$("blockquote").html("<span>no quote</span>");
+};
+
+
+$("#button2").click(question6);
 
 //SECTION 3: This section is more difficult. Try your hand at the next 3 questions.
 
